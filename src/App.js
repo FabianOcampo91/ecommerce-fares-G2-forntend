772ead8b933cd0e9e5ede0ff5b613d86e65152b3
FabianOcampo91 <faby.ocampo91@gmail.com>
2022-05-09 12:00:39 -0300

    chore: master config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   )
 }
 
